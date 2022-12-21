021d4e1c36d5282a1727c6b88e627bdee64162e3
Manoj Rathod <myraysever@gmail.com>
2022-12-21 19:21:32 +0530

    added landing page folder

---
+++ Components/LandingPage/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const index = () => {
+  return <div>this is about</div>;
+};
+
+export default index;
+// this is about
--- pages/home.js
+++ pages/home.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const home = () => {
-  return <div>This is home page</div>;
+  return <div></div>;
 };
 
 export default home;
